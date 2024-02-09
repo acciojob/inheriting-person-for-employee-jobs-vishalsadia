af815cb6fb8be411aa5ba2be5df2c42e1871f08c
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2024-02-09 17:04:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// complete this js code
 function Person(name, age) {
-	this.name=name;
-	this.age=age;
+  this.name = name;
+  this.age = age;
 }
-Person.prototype.greet=function {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	
+
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 
-
 function Employee(name, age, jobTitle) {
-
-	Person.call(this,name,age);
-	this.jobTitle=jobTitle;
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
+
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
